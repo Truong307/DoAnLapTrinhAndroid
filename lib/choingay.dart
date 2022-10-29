5b1bdf25846a07cd9f9e0b537a70726e95fd183f
ChauKimSon <0306201377@caothang.edu.vn>
2022-10-29 12:12:49 +0700

    Thêm file choingay,guithachdau,nutchoingay,thongbao,tintuc,trangchu

---
+++ lib/choingay.dart
@@ -0,0 +1,430 @@
+import 'package:doan_lt_didong/trangchu.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ChoiNgay extends StatefulWidget {
+  @override
+  State<ChoiNgay> createState() => ChoiNgay_State();
+}
+
+class ChoiNgay_State extends State<ChoiNgay> {
+  //=========================================//
+  Text _text(String lable) {
+    return Text(
+      lable,
+      textAlign: TextAlign.center,
+      style: GoogleFonts.lato(
+        textStyle: Theme.of(context).textTheme.headline4,
+        fontSize: 15,
+        color: Colors.black,
+        fontWeight: FontWeight.w900,
+      ),
+    );
+  }
+
+  //==========================================//
+  @override
+  Widget build(BuildContext context) {
+    //==================================================//
+    Widget backButton = Container(
+      child: Row(
+        children: [
+          Container(
+            child: IconButton(
+              splashRadius: 24,
+              tooltip: 'Trở về',
+              //Xử trở về trang chủ
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TrangChu()));
+              },
+              //========================//
+              icon: Image.asset('images/undo.png'),
+              iconSize: 40,
+            ),
+          ),
+        ],
+      ),
+    );
+    //====================================//
+    Widget optionButton = Container(
+      margin: const EdgeInsets.only(top: 150),
+      child: Column(
+        children: <Widget>[
+          // Nút độ khó
+          Container(
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ))),
+              onPressed: () {
+                showDialog(
+                  barrierColor: Colors.transparent,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () => Navigator.pop(context),
+                            icon: const Icon(
+                              Icons.backspace_outlined,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 35,
+                          ),
+                          const Text(
+                            "CHẾ ĐỘ",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      content: Container(
+                        height: 165,
+                        child: Column(
+                          children: [
+                            Container(
+                              child: InkWell(
+                                onTap: () {},
+                                onHover: (val) {
+                                  setState(() {
+                                    //print("Val--->{}$val");
+                                    //isHoverEasy = val;
+                                  });
+                                },
+                                child: ElevatedButton(
+                                  onPressed: () {}, // Chưa xử lý
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                      top: 5,
+                                      bottom: 5,
+                                      left: 45,
+                                      right: 45,
+                                    ),
+                                    child: Text(
+                                      'Dễ',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.all(10),
+                              child: ElevatedButton(
+                                onPressed: () {}, // Chưa xử lý
+                                child: const Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 5,
+                                    left: 3,
+                                    right: 3,
+                                  ),
+                                  child: Text(
+                                    "Trung Bình",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: ElevatedButton(
+                                onPressed: () {}, // Chưa xử lý
+                                child: const Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 5,
+                                    left: 40,
+                                    right: 40,
+                                  ),
+                                  child: Text(
+                                    "Khó",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }, //Xử lý
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Text(
+                  'Độ Khó',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Nút lĩnh vực
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  )),
+                  shadowColor: MaterialStateProperty.all<Color>(Colors.black)),
+              onPressed: () {
+                showDialog(
+                  barrierColor: Colors.transparent,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () => Navigator.pop(context),
+                            icon: const Icon(Icons.backspace_outlined),
+                          ),
+                          const SizedBox(
+                            width: 35,
+                          ),
+                          const Text(
+                            "CHẾ ĐỘ",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      content: Container(
+                        height: 175,
+                        child: Column(
+                          children: [
+                            Container(
+                              child: Row(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(
+                                          top: 10,
+                                          bottom: 10,
+                                          left: 14,
+                                          right: 14,
+                                        ),
+                                        child: Text(
+                                          'Lịch Sử',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(
+                                          top: 10,
+                                          bottom: 10,
+                                          left: 17,
+                                          right: 17,
+                                        ),
+                                        child: Text(
+                                          'Địa Lí',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(
+                                          top: 10,
+                                          bottom: 10,
+                                          left: 5,
+                                          right: 5,
+                                        ),
+                                        child: Text(
+                                          'Pháp Luật',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(
+                                          top: 10,
+                                          bottom: 10,
+                                          left: 14,
+                                          right: 14,
+                                        ),
+                                        child: Text(
+                                          'Xã Hội',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(top: 1),
+                                        child: Text(
+                                          'Khoa Học -\n Công Nghệ',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.all(5),
+                                    child: ElevatedButton(
+                                      onPressed: () {},
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(
+                                          top: 9,
+                                          bottom: 9,
+                                          left: 30,
+                                          right: 30,
+                                        ),
+                                        child: Text(
+                                          '...',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }, //Chưa xử lý
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  left: 13,
+                  right: 13,
+                  top: 10,
+                  bottom: 10,
+                ),
+                child: Text(
+                  'Lĩnh Vực',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Nút Bắt đầu
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shadowColor: Colors.black,
+                elevation: 20,
+              ),
+              onPressed: () {}, //Chưa xử lý
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  left: 24,
+                  right: 24,
+                  top: 20,
+                  bottom: 20,
+                ),
+                child: Text(
+                  'Bắt Đầu',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+//============================================================//
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: backButton,
+        backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              optionButton,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/thongbao.dart
@@ -0,0 +1,288 @@
+import 'package:doan_lt_didong/trangchu.dart';
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+class ThongBao_App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Thông Báo",
+      home: ThongBao_Home(),
+    );
+  }
+}
+
+class ThongBao_Home extends StatefulWidget {
+  @override
+  State<ThongBao_Home> createState() => ThongBao_State();
+}
+
+class ThongBao_State extends State<ThongBao_Home> {
+  @override
+  Widget build(BuildContext context) {
+//======================================================//
+    Widget headSection = Container(
+      child: Row(
+        children: [
+          Container(
+            child: IconButton(
+              splashRadius: 24,
+              tooltip: 'Trở về',
+              //Xử trở về trang chủ
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TrangChu()));
+              },
+              //========================//
+              icon: Image.asset('images/undo.png'),
+              iconSize: 40,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 80),
+            child: const Text(
+              "Thông báo",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+//======================================================//
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      onPrimary: Colors.black87,
+      primary: Colors.blue,
+      minimumSize: const Size(88, 36),
+      padding: const EdgeInsets.only(
+        left: 30,
+        right: 30,
+        top: 15,
+        bottom: 15,
+      ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+    );
+
+//========================================================//
+    Widget optionSection = Container(
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: () {}, // Chưa xử lý
+              child: const Text(
+                'Tất cả',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: () {}, // Chưa xử lý
+              child: const Text(
+                'Chưa đọc',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+//======================================================//
+    Widget contentSection = Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      child: Column(
+        children: [
+          Card(
+            color: Colors.grey[300],
+            margin: const EdgeInsets.only(
+              top: 15,
+              left: 15,
+              right: 15,
+            ),
+            child: ListTile(
+              title: const Text(
+                'User đã gửi lời mời kết bạn',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              subtitle: const Text(
+                '1 phút trước',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    child: TextButton(
+                      onPressed: () {}, // Chưa xử lý
+                      child: Text(
+                        'Chấp nhận',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            const MaterialStatePropertyAll<Color>(Colors.green),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: TextButton(
+                      onPressed: () {}, // Chưa xử lý
+                      child: const Text(
+                        'Từ chối',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            const MaterialStatePropertyAll<Color>(Colors.red),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          //===========//
+          Card(
+            color: Colors.grey[300],
+            margin: const EdgeInsets.only(
+              top: 15,
+              left: 15,
+              right: 15,
+            ),
+            child: ListTile(
+              title: const Text(
+                'User đã gửi lời mời kết bạn',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              subtitle: const Text(
+                '30 phút trước',
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    child: TextButton(
+                      onPressed: () {}, // Chưa xử lý
+                      child: const Text(
+                        'Chấp nhận',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            const MaterialStatePropertyAll<Color>(Colors.green),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: TextButton(
+                      onPressed: () {}, // Chưa xử lý
+                      child: const Text(
+                        'Từ chối',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            const MaterialStatePropertyAll<Color>(Colors.red),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+//======================================================//
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            headSection,
+          ],
+        ),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              optionSection,
+              contentSection,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
